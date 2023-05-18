e3c718f07e1d995aeea4e55b439c16e9abaaea55
Renato Angelo <renatoangelo@gmail.com>
2023-05-18 11:19:05 -0300

    Alterações de Menu

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brand" href="index.html">
-                    Festava Live
+                    Work in Tech
                 </a>
 
                 <a href="ticket.html" class="btn custom-btn d-lg-none ms-auto me-4">Buy Ticket</a>
@@ -33,35 +33,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_1">Home</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_2">About</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_3">Artists</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_4">Schedule</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_5">Pricing</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_6">Contact</a>
-                        </li>
-                    </ul>
-
-                    <a href="ticket.html" class="btn custom-btn d-lg-block d-none">Buy Ticket</a>
-                </div>
+                <?php include("menu.php"); ?>
             </div>
         </nav>
 
---
+++ menu.php
@@ -0,0 +1,29 @@
+<div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
+      <li class="nav-item">
+          <a class="nav-link click-scroll" href="#section_1">Home</a>
+      </li>
+
+      <li class="nav-item">
+          <a class="nav-link click-scroll" href="#section_2">Sobre</a>
+      </li>
+
+      <li class="nav-item">
+          <a class="nav-link click-scroll" href="#section_3">Mentores e Empresas</a>
+      </li>
+
+      <li class="nav-item">
+          <a class="nav-link click-scroll" href="#section_4">Cronograma</a>
+      </li>
+
+      <li class="nav-item">
+          <a class="nav-link click-scroll" href="#section_5">Pricing</a>
+      </li>
+
+      <li class="nav-item">
+          <a class="nav-link click-scroll" href="#section_6">Inscrição</a>
+      </li>
+  </ul>
+
+  <a href="ticket.html" class="btn custom-btn d-lg-block d-none">Inscreva-se</a>
+</div>
