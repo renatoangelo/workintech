0b5bd39c324d9c7632bdc0f3ed04baf75a683c65
pedroHenriqueLomba <p.henrique.lomba@gmail.com>
2023-05-19 10:14:34 -0300

    Lomba

--- index.php
+++ index.php
@@ -68,14 +68,15 @@
     <section class="about-section section-padding" id="section_2">
       <div class="container">
         <div class="row">
+          <h2 class="text-white mb-4">Work in Tech</h2>
+        </div>
 
+        <div class="row">
           <div class="col-lg-6 col-12 mb-4 mb-lg-0 d-flex align-items-center">
             <div class="services-info">
-              <h2 class="text-white mb-4">Work in Tech</h2>
-
               <p class="text-white">Um evento único em que várias empresas renomadas apresentam oportunidades de emprego nas áreas de programação e TI. Além de palestras inspiradoras para auxiliar os alunos a explorarem suas paixões e se encontrarem. Não perca essa chance de iniciar sua carreira com sucesso!
               </p>
-
+              
               <h6 class="text-white mt-4">Vantagens do evento</h6>
 
               <p class="text-white">Acesso direto a diversas empresas do setor de TI, além de palestras inspiradoras que auxiliam os alunos a explorarem diferentes áreas e se prepararem para o mercado de trabalho</p>
@@ -86,10 +87,9 @@
             </div>
           </div>
 
-          <div class="col-lg-6 col-12">
+          <div class="col-lg-6 col-12 d-flex align-items-center">
             <div class="about-text-wrap">
               <img src="images/logo-roxo.png" class="about-image img-fluid">
-
             </div>
           </div>
 
