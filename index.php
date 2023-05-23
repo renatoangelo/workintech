8df7489d2b2c775d9df27d9f78aa9e2be1f7d444
Renato Angelo <renatoangelo@gmail.com>
2023-05-23 15:05:30 -0300

    Atualizacao Index

--- index.php
+++ index.php
@@ -145,42 +145,11 @@
 
             <nav class="d-flex justify-content-center">
               <div class="nav nav-tabs align-items-baseline justify-content-center" id="nav-tab" role="tablist">
-                <button class="nav-link active" id="nav-ContactForm-tab" data-bs-toggle="tab" data-bs-target="#nav-ContactForm" type="button" role="tab" aria-controls="nav-ContactForm" aria-selected="false">
-                  <h5>Inscrever-se</h5>
-                </button>
+                
               </div>
             </nav>
 
-            <!--<div class="tab-content shadow-lg mt-5" id="nav-tabContent">
-              <div class="tab-pane fade show active" id="nav-ContactForm" role="tabpanel" aria-labelledby="nav-ContactForm-tab">
-                <form class="custom-form contact-form mb-5 mb-lg-0" action="#" method="post" role="form">
-                  <div class="contact-form-body">
-                    <div class="row">
-                      <div class="col-lg-6 col-md-6 col-12">
-                        <input type="text" name="contact-name" id="contact-name" class="form-control" placeholder="Full name" required>
-                      </div>
-
-                      <div class="col-lg-6 col-md-6 col-12">
-                        <input type="email" name="contact-email" id="contact-email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required>
-                      </div>
-                    </div>
-
-                    <input type="text" name="contact-company" id="contact-company" class="form-control" placeholder="Company" required>
-
-                    <textarea name="contact-message" rows="3" class="form-control" id="contact-message" placeholder="Message"></textarea>
-
-                    <div class="col-lg-4 col-md-10 col-8 mx-auto">
-                      <button type="submit" class="form-control">Send message</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-
-              <div class="tab-pane fade" id="nav-ContactMap" role="tabpanel" aria-labelledby="nav-ContactMap-tab">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17399.602722835334!2d-51.938714581525524!3d-23.4461517991622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e81a413a9c9463%3A0x4e3ee356ad1bd6c3!2sUniCesumar%20-%20Maring%C3%A1!5e0!3m2!1spt-BR!2sbr!4v1684459563530!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                
-              </div>
-            </div>-->
+            
           </div>
 
         </div>
