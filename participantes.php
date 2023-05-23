432f40251c556b3ec5c1293c9509dacc856ac0f8
Renato Angelo <renatoangelo@gmail.com>
2023-05-23 19:17:59 -0300

    participantes.php

--- participantes.php
+++ participantes.php
@@ -1,177 +1,80 @@
-<div class="col-12 text-center">
-  <h5 class="mb-4">Mentores</h5>
-</div>
+<h5>Mentores e Empresas</h5>
 
+
+<div class="row">
 <!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/clayton.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Clayton Passos
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Senior Software Solutions Architect - Subject Matter Expert</a>
-      </p>
+  <div class="col-lg-3 col-12 mb-2">
+    <div class="card bg-verde h-100">
+      <img src="images/palestrantes/clayton.jpg" class="card-img-top">
+      <div class="card-body card-body-participante">
+        <h5 class="card-title">Clayton Passos</h5>
+        <p class="text-azul">Senior Software Solutions Architect - Subject Matter Expert</p>
+      </div>
     </div>
   </div>
-</div>
 <!-- FIM PALESTRANTE -->
 
 <!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/ivo.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Ivo Roberto Batistela
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Senior Consultant Developer</a>
-      </p>
+  <div class="col-lg-3 col-12 mb-2">
+    <div class="card bg-verde h-100">
+      <img src="images/palestrantes/ivo.jpg" class="card-img-top">
+      <div class="card-body card-body-participante">
+        <h5 class="card-title">Ivo Roberto Batistela</h5>
+        <p class="text-azul">Senior Consultant Developer</p>
+      </div>
     </div>
   </div>
-<<<<<<< HEAD
+<!-- FIM PALESTRANTE -->
+
+<!--PALESTRANTE -->
+  <div class="col-lg-3 col-12 mb-2">
+    <div class="card bg-verde h-100">
+      <img src="images/palestrantes/francisco.jpg" class="card-img-top">
+      <div class="card-body card-body-participante">
+        <h5 class="card-title">Francisco Thiago de Almeida</h5>
+        <p class="text-azul">Arquiteto de Software</p>
+      </div>
+    </div>
+  </div>
+<!-- FIM PALESTRANTE -->
+
+<!--PALESTRANTE -->
+<div class="col-lg-3 col-12 mb-2">
+    <div class="card bg-verde h-100">
+      <img src="images/palestrantes/frank.jpg" class="card-img-top">
+      <div class="card-body card-body-participante">
+        <h5 class="card-title">Frank Aguilieri</h5>
+        <p class="text-azul">Diretor de Projetos de TI e Segurança da Informação</p>
+      </div>
+    </div>
+  </div>
+<!-- FIM PALESTRANTE -->
+</div>
+
+<h5>Palestras e Oficinas</h5>
+
+<div class="row">
+  <!--PALESTRANTE -->
+  <div class="offset-lg-3 col-lg-3 col-12">
+    <div class="card bg-verde">
+      <img src="images/palestrantes/amanda.jpg" class="card-img-top">
+      <div class="card-body card-body-participante">
+        <h5 class="card-title">Amanda Tambara</h5>
+        <p class="text-azul">Jornalista especialista em comunicação digital e e-branding em constante atualização.</p>
+      </div>
+    </div>
+  </div>
+  <!-- FIM PALESTRANTE -->
+
+  <!--PALESTRANTE -->
+  <div class="col-lg-3 col-12">
+    <div class="card bg-verde h-100">
+      <img src="images/palestrantes/renato.jpg" class="card-img-top" alt="...">
+      <div class="card-body card-body-participante">
+        <h5 class="card-title">Renato Angelo</h5>
+        <p class="text-azul">Diretor de Marketing - Senior Developer</p>
+      </div>
+    </div>
+  </div>
   <!-- FIM PALESTRANTE -->
 </div>
-<!-- Aqui vai um comentário final 1557 -->
-
-=======
-</div>
-<!-- FIM PALESTRANTE -->
-
-<!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/francisco.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Francisco Thiago de Almeida
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Arquiteto de Software</a>
-      </p>
-    </div>
-  </div>
-</div>
-<!-- FIM PALESTRANTE -->
-
-<!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/frank.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Frank Aguilieri
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Diretor de Projetos de TI e Segurança da Informação</a>
-      </p>
-    </div>
-  </div>
-</div>
-<!-- FIM PALESTRANTE -->
-
-
-
-
-
-<div class="col-12 text-center">
-  <h5 class="mb-4">Palestras e Oficinas</h5>
-</div>
-
-<!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/amanda.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Amanda Tambara
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Jornalista especialista em comunicação digital e e-branding em constante atualização.</a>
-      </p>
-
-      <p class="mb-0">
-        <strong>Linked in:</strong>
-        <a href="#">Amanda Tambara</a>
-      </p>
-    </div>
-  </div>
-</div>
-<!-- FIM PALESTRANTE -->
-
-<!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/tiago.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Tiago Freire
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Analista de Negócios</a>
-      </p>
-    </div>
-  </div>
-</div>
-<!-- FIM PALESTRANTE -->
-
-<!--PALESTRANTE -->
-<div class="col-lg-3 col-12">
-  <div class="artists-thumb">
-    <div class="artists-image-wrap">
-      <img src="images/palestrantes/renato.jpg" class="artists-image img-fluid">
-    </div>
-
-    <div class="artists-hover">
-      <p>
-        <strong>Nome:</strong>
-        Renato Angelo
-      </p>
-
-      <p class="mb-0">
-        <strong>Função:</strong>
-        <a href="#">Diretor de Marketing - Senior Developer</a>
-      </p>
-    </div>
-  </div>
-</div>
-<!-- FIM PALESTRANTE -->
->>>>>>> 3acab18 (resolvendo)
