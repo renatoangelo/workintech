7006def7d16cb125b89e20615cec2a2a0e55b974
Renato Angelo <renatoangelo@gmail.com>
2023-05-24 10:38:25 -0300

    Alteracoes basicas

--- tabela-salas.php
+++ tabela-salas.php
@@ -18,22 +18,22 @@
       </tr>
       <tr>
         <td scope="row">19h15</td>
-        <td style="background-color: var(--secondary-color);"><h4 style="color: white">Matera</h4>[Empresa / Mentoria]</td>
-        <td style="background-color: var(--secondary-color);"><h4 style="color: white">Lode</h4>[Empresa / Mentoria]</td>
-        <td style="background-color: var(--primary-color);"><h4>Mentoria Internacional</h4></td>
+        <td style="background-color: var(--secondary-color);"><h4 style="color: white">Matera</h4>[Empresa]</td>
+        <td style="background-color: var(--secondary-color);"><h4 style="color: white">Lode</h4>[Empresa]</td>
+        <td style="background-color: var(--primary-color);"><h4>Mentoria Internacional[Douglas Bianchezzi]</h4></td>
       </tr>
       
       <tr>
         <td scope="row">20h30</td>
-        <td style="background-color: var(--secondary-color);"><h4 style="color: white">Elotech</h4>[Empresa / Mentoria]</td>
-        <td style="background-color: var(--secondary-color);"><h4 style="color: white">RH Accion</h4>[Empresa / Mentoria]</td>
-        <td style="background-color: var(--primary-color)"><h4>Mentoria Internacional</h4></td>
+        <td style="background-color: var(--secondary-color);"><h4 style="color: white">Elotech</h4>[Empresa]</td>
+        <td style="background-color: var(--secondary-color);"><h4 style="color: white">RH Accion</h4>[Empresa]</td>
+        <td style="background-color: var(--primary-color)"><h4>Mentoria Internacional</h4>[William Azevedo]</td>
       </tr>
       
       <tr>
         <td scope="row">21h45</td>
-        <td style="background-color: var(--secondary-color);"><h4 style="color: white">DB1</h4>[Empresa / Mentoria]</td>
-        <td style="background-color: var(--secondary-color);"><h4 style="color: white">ItalenTI</h4>[Empresa / Mentoria]</td>
+        <td style="background-color: var(--secondary-color);"><h4 style="color: white">DB1</h4>[Empresa]</td>
+        <td style="background-color: var(--secondary-color);"><h4 style="color: white">ItalenTI</h4>[Empresa]</td>
         <td style="background-color: var(--white-color);">-</td>
       </tr>
 
